0df1841ecd5a46bc9e37171fe21e7380383f9497
Panithan Bunrong <>
2025-06-03 00:17:30 +0700

    Add link a report

---
+++ tests/example002.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+// import { initializeDB } from '../db/initialize';
+// import { queryDB } from '../db/query';
+import { LoginPage } from '../pages/LoginPage';
+import * as loginData from '../data-test/loginData.json';
+const testData = loginData;
+
+test.beforeAll(async () => {
+  // Initialize database before running tests
+  // await initializeDB();
+});
+
+test('has title', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await page.goto(process.env.URL!);
+  await loginPage.login(testData.validAdmin.username, testData.validAdmin.password);
+  // await loginPage.login('Admin', 'admin123');
+  await page.waitForTimeout(5000);
+
+  // const result: any = await queryDB('SELECT * FROM users WHERE username = ?', ['Admin']);
+  // console.log('📦 User info from DB:', result);
+});
--- utils/notifications/globalTeardownNotifier.ts
+++ utils/notifications/globalTeardownNotifier.ts
@@ -1,18 +1,21 @@
 // global-teardown.ts
-import { sendSimpleTeamsMessage } from './sendToTeams';
+import { sendSimpleTeamsMessage } from "./sendToTeams";
 
-const webhookUrl = 'https://bdmsgroup.webhook.office.com/webhookb2/37c6a24a-aa0e-4d45-adea-fa8400cbf5e4@325c40be-6d2b-4006-b2c5-078947c856d2/IncomingWebhook/51643aae01dc4242948769a5ac358336/d00d6fcd-747f-47b7-9fe1-55a34064c1fe/V2Q0mIuRhdLMJaYoFje039Zy4f7SIa-jgMbafbwv1bBKs1'
+const webhookUrl =
+  "https://bdmsgroup.webhook.office.com/webhookb2/37c6a24a-aa0e-4d45-adea-fa8400cbf5e4@325c40be-6d2b-4006-b2c5-078947c856d2/IncomingWebhook/51643aae01dc4242948769a5ac358336/d00d6fcd-747f-47b7-9fe1-55a34064c1fe/V2Q0mIuRhdLMJaYoFje039Zy4f7SIa-jgMbafbwv1bBKs1";
 // const webhookUrl = 'https://bdmsgroup.webhook.office.com/webhookb2/16bf4760-41f5-46de-aceb-3b2f5598b92e@325c40be-6d2b-4006-b2c5-078947c856d2/IncomingWebhook/7da855b84d744b4ab853ed9ec0976c6f/d00d6fcd-747f-47b7-9fe1-55a34064c1fe/V2bb51Gt3Qu9HwsvgE631gT9Du3W1bO13-2Owlgl1FBSw1';
 
 async function globalTeardown() {
-  const summaryText = `📊 *SSH-Backoffice Test Summary*\n\n` +
-                      `**Total:** Hidden\n` +
-                      `✅ Passed: 10\n❌ Failed: 0\n⏭️ Skipped: 0`;
+  const summaryText =
+    `📊 *SSH-Backoffice Test Summary*\n\n` +
+    `Report: https://mutto1003.github.io/playwright-ui-typescript/\n\n` +
+    `**Total:** Hidden\n` +
+    `✅ Passed: 10\n❌ Failed: 0\n⏭️ Skipped: 0`;
 
   if (webhookUrl) {
     await sendSimpleTeamsMessage(webhookUrl, summaryText);
   } else {
-    console.warn('⚠️ TEAMS_WEBHOOK_URL not set, skipping Teams notification.');
+    console.warn("⚠️ TEAMS_WEBHOOK_URL not set, skipping Teams notification.");
   }
 }
 
