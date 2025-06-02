4f5d91575a957898d10cbc7831551714fd4515f3
Panithan Bunrong <>
2025-06-02 18:02:46 +0700

    add data-test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 // import { initializeDB } from '../db/initialize';
 // import { queryDB } from '../db/query';
 import { LoginPage } from '../pages/LoginPage';
+import * as loginData from '../data-test/loginData.json';
+const testData = loginData;
 
 test.beforeAll(async () => {
   // Initialize database before running tests
@@ -12,7 +14,8 @@ test('has title', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await page.goto(process.env.URL!);
-  await loginPage.login('Admin', 'admin123');
+  await loginPage.login(testData.validAdmin.username, testData.validAdmin.password);
+  // await loginPage.login('Admin', 'admin123');
   await page.waitForTimeout(5000);
 
   // const result: any = await queryDB('SELECT * FROM users WHERE username = ?', ['Admin']);
